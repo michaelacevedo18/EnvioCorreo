ebc1b18c4771147bf2be9aa26d5aab2f81806c61
Brander <slook0018@gmail.com>
2022-07-08 13:33:46 -0500

    Agregar archivos de proyecto.

---
+++ EnvioCorreo/Program.cs
@@ -0,0 +1,83 @@
+﻿using Amazon;
+using Amazon.SimpleEmail;
+using Amazon.SimpleEmail.Model;
+using System;
+using System.Collections.Generic;
+
+namespace EnvioCorreo
+{
+    class Program
+    {
+        
+        static readonly string senderAddress = "slook0018@gmail.com";        
+        static readonly string receiverAddress = "michaelacevedoruiz48@gmail.com";        
+                
+        static readonly string subject = "Amazon SES test (AWS SDK for .NET)";
+                
+        static readonly string textBody = "Amazon SES Test (.NET)\r\n"
+                                        + "This email was sent through Amazon SES "
+                                        + "using the AWS SDK for .NET.";
+
+        // The HTML body of the email.
+        static readonly string htmlBody = @"<html>
+<head></head>
+<body>
+  <h1>Amazon SES Test (AWS SDK for .NET)</h1>
+  <p>This email was sent with
+    <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+    <a href='https://aws.amazon.com/sdk-for-net/'>
+      AWS SDK for .NET</a>.</p>
+</body>
+</html>";
+
+        static void Main(string[] args)
+        {
+            // Replace USWest2 with the AWS Region you're using for Amazon SES.
+            // Acceptable values are EUWest1, USEast1, and USWest2.
+            using (var client = new AmazonSimpleEmailServiceClient(RegionEndpoint.USWest2))
+            {
+                var sendRequest = new SendEmailRequest
+                {
+                    Source = senderAddress,
+                    Destination = new Destination
+                    {
+                        ToAddresses =
+                        new List<string> { receiverAddress }
+                    },
+                    Message = new Message
+                    {
+                        Subject = new Content(subject),
+                        Body = new Body
+                        {
+                            Html = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = htmlBody
+                            },
+                            Text = new Content
+                            {
+                                Charset = "UTF-8",
+                                Data = textBody
+                            }
+                        }
+                    }                   
+                };
+                try
+                {
+                    Console.WriteLine("Sending email using Amazon SES...");
+                    var response = client.SendEmailAsync(sendRequest);
+                    Console.WriteLine("The email was sent successfully.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("The email was not sent.");
+                    Console.WriteLine("Error message: " + ex.Message);
+
+                }
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
